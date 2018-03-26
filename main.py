8faf5118b448a837b35626fa680b6453ccaa3d57
MONIK RAJ BEHERA <bmonikraj@gmail.com>
2018-03-26 18:29:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 21:23:11 2018
+
+@author: MONIK RAJ
+"""
+
+#Module importingg
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from keras.utils import np_utils
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Activation, Conv2D,MaxPooling2D, Flatten
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Reading dataset
+data = pd.read_csv('diabetes.csv')
+
+#Separation of Y and X from Dataset 
+Y = np.array(data['Outcome'])
+del data['Outcome']
+X = np.array(data)
+
+'''
+# Compute the correlation matrix
+corr = data.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+sns.pairplot(data)
+
+sns.jointplot(x="BMI", y="Glucose", data=data, kind = 'hex', 
+              cmap= 'hot', 
+              size=6).savefig("d.png")
+'''
+
+#Splitting of dataset into Training set and testing set (80% and 20% respectively)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+#Single layer architecture
+model = Sequential()
+model.add(Dense(32,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(64,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(64,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(128,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(128,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(256,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(256,input_shape=(X_train.shape[1],)))
+model.add(Activation('relu'))
+model.add(Dense(2))
+model.add(Activation('softmax'))
+
+print(model.summary())
+
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer="sgd",metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=128, epochs=100, verbose=1, validation_data=(X_test, Y_test))
+loss, accuracy = model.evaluate(X_test,Y_test, verbose=0)
+print("Loss : "+str(loss))
+
+print("Accuracy :"+str(accuracy*100.0))
